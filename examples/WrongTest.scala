d6138211c5c6e06d73e0d038638249925d18678c
Haley Owsianko <haley.owsianko@gmail.com>
2018-10-14 14:57:13 +0200

    Implement LL(1) grammar for Amy

--- examples/WrongTest.scala
+++ examples/WrongTest.scala
@@ -1,5 +1,14 @@
 object h
 {
-  abstract class bepis
-  case class hi(xd: Boolean, lol: Int) extends bepis
+  if(!7 || hello < 5 == 5 && 5)
+  {
+    val lol: Int = 7;
+    5;
+    6;
+    5
+  }
+  else
+  {
+    "lmao"
+  }
 }
